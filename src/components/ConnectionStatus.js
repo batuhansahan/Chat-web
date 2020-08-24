3d35c93d829714ca2c80d23111a07efdf09cb043
Batuhansahan <batuhansahan@trakya.edu.tr>
2020-08-24 20:33:57 +0300

    added signature

--- src/components/ConnectionStatus.js
+++ src/components/ConnectionStatus.js
@@ -1,28 +1,49 @@
 import React from "react";
 import "../styles/connectionStatus.css";
-import {BiUser,BiMessageDetail} from 'react-icons/bi'
-import {FiMessageCircle, FiUser, FiLogOut} from 'react-icons/fi'
+import { BiUser, BiMessageDetail } from "react-icons/bi";
+import { FiMessageCircle, FiUser, FiLogOut, FiGithub } from "react-icons/fi";
 
-function connectionStatus({ isWs, isRoomJoined, room, username, exitRoom}) {
+function connectionStatus({ isWs, isRoomJoined, room, username, exitRoom }) {
   return (
     <div className="Status">
       <div className="flex-row">
-      <div className="StatusBox">
-        <div className="aligncenter"><FiMessageCircle size={22} style={{marginRight:5}}/>{isRoomJoined ? room : "Lobby"}</div>
+        <div className="StatusBox">
+          <div className="aligncenter">
+            <FiMessageCircle size={22} style={{ marginRight: 5 }} />
+            {isRoomJoined ? room : "Lobby"}
+          </div>
+        </div>
+        {isRoomJoined && (
+          <div className="StatusBox">
+            <div className="aligncenter">
+              <FiUser size={22} style={{ marginRight: 5 }} /> {username}
+            </div>
+          </div>
+        )}
+        {isRoomJoined && (
+          <div className="StatusBox" onClick={() => exitRoom()}>
+            <div className="aligncenter red">
+              <FiLogOut color={"red"} size={22} style={{ marginRight: 5 }} />{" "}
+              Exit
+            </div>
+          </div>
+        )}
       </div>
-      {isRoomJoined &&
-      <div className="StatusBox">
-        <div className="aligncenter"><FiUser size={22} style={{marginRight:5}}/> {username}</div>
-      </div>
-      }
-      {isRoomJoined && 
-      <div className="StatusBox" onClick={()=>exitRoom()}>
-        <div className="aligncenter red"><FiLogOut color={"red"} size={22} style={{marginRight:5}}/> Exit</div>
-      </div>}
-      </div>
-      <div className="StatusBox">
-        <div className={isWs+'Status'}>{isWs} to server</div>
-        <div className={isWs+"Dot"}></div>
+      <div className="Status">
+        <a
+          className="StatusBox"
+          style={{ textDecoration: "none" }}
+          href="https://github.com/batuhansahan"
+        >
+          <div className="aligncenter">
+            <FiGithub size={22} style={{ marginRight: 5 }} />
+            Batuhan Şahan
+          </div>
+        </a>
+        <div className="StatusBox">
+          <div className={isWs + "Status"}>{isWs} to server</div>
+          <div className={isWs + "Dot"}></div>
+        </div>
       </div>
     </div>
   );
